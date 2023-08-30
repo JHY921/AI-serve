fe1a0c3eac24ee690590fefc79eced7eca5ee052
JHY921 <452212248@qq.com>
2023-08-30 23:27:18 +0800

    city数据换成sex

--- app.py
+++ app.py
@@ -45,14 +45,14 @@ def userinfo():
     name = data.get('name')
     birth = data.get('birth')
     degree = data.get('degree')
-    city = data.get('city')
+    city = data.get('sex')
     user_id = data.get('id')
     update_data = {
         '$set': {
             'name': name,
             'birth': birth,
             'degree': degree,
-            'city': city,
+            'sex': city,
         }
     }
     users = mongo.db.users
